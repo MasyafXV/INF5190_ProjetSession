3dcc97da93f8679663f9fcdb35e9238f807e3fba
Trafalgar <masyyaf@gmail.com>
2020-11-26 10:25:16 -0500

    Update ListCoursesAction.java

--- src/com/pronatation/Courses/ListCoursesAction.java
+++ src/com/pronatation/Courses/ListCoursesAction.java
@@ -1,6 +1,14 @@
 package com.pronatation.Courses;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -16,17 +24,40 @@ public class ListCoursesAction extends ActionSupport {
 	public String execute() {
 
 		System.out.println("\nListing courses");
+		String url = "http://localhost:8080/services/webapi/";
+		String url_param = "course/getAllCourses/";
+		
+		System.out.println("\nConnection to  " + url+url_param);
+		
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url+url_param))
+                .build();
 
-		CourseBusiness courseBusiness = new CourseBusiness();
-
-		ArrayList<CourseDTO> listCoursesDTO = courseBusiness.getAllCourses();
-
+        HttpResponse<String>  response = null;
+		try {
+			response = client.send(request,
+			        HttpResponse.BodyHandlers.ofString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		this.coursesList = new ArrayList<>();
 
-		for (int i = 0; i < listCoursesDTO.size(); i++) {
-			this.coursesList.add(new CourseBean(listCoursesDTO.get(i).getSessionCode(),
-					listCoursesDTO.get(i).getCourseLevel(), listCoursesDTO.get(i).getDescription()));
-		}
+        JSONArray coursesJSON = new JSONArray(response.body());
+        for (int i = 0; i < coursesJSON.length(); i++) {
+        	JSONObject courseJSON= coursesJSON.getJSONObject(i);
+            System.out.println("json body : "+courseJSON);
+            
+			this.coursesList.add(new CourseBean(courseJSON.getString("sessionCode"),
+					courseJSON.getString("courseLevel"), courseJSON.getString("description")));
+
+        }
+        
 
 		return SUCCESS;
 	}
